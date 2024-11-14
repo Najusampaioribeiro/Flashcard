c5f0795505212acd0211ae4c47f474e893d12714
Najú <168193432+Najusampaioribeiro@users.noreply.github.com>
2024-11-13 21:16:47 -0300

    inserindo Arquivos

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cultura Pop',
+    'O que é "Taylorismo"?',
+    'São albuns escritos pela cantora estadunidense Taylor Swift.'
+)
+
+criaCartao(
+    'You are your own kid',
+    'O que são friendship Bracelets?',
+    ' São Pulseiras feitas a mão pelos fãs da cantora e compartilhados entre eles, como forma de fortificação de laços.'
+)
+
+criaCartao(
+    'Superstição',
+    'Por que o número dela preferido é o 13?',
+    'O número 13 acompanhou Taylor em diversos momentos de sua vida, como o exemplo do lançamento de seu primeiro album e a própria data de seu aniversário.'
+)
+
+criaCartao(
+    'Curiosidades',
+    'Curiosidades',
+    'Taylor é muito fã da série Grey´s Anatomy. Por isso, uma de suas gatas se chama Meredith em homenagem a personagem principal da série.'
+)
